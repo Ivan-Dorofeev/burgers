d49b821accbf6d2397e0c34bf79e1608b052f365
Ivan Dorofeev <axxel123@rambler.ru>
2023-03-14 09:11:01 +0300

    Add data validation for register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import JsonResponse
 from django.templatetags.static import static
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -90,6 +91,16 @@ def register_order(request):
     try:
         get_orders = request.data
         print('orders = ', get_orders)
+        print('orders.type = ', type(get_orders))
+
+        if not isinstance(get_orders['products'], list):
+            return Response('products: Ожидался list со значениями, но был получен "str"', status=status.HTTP_400_BAD_REQUEST)
+        elif get_orders['products'] == 'null':
+            return Response('products: Это поле не может быть пустым',status=status.HTTP_400_BAD_REQUEST)
+        elif not get_orders['products']:
+            return Response('products: Этот список не может быть пустым', status=status.HTTP_400_BAD_REQUEST)
+        elif not 'products' in get_orders.keys():
+            return Response('products: Обязательное поле', status=status.HTTP_400_BAD_REQUEST)
 
         order = Order.objects.create(
             client_name=get_orders['firstname'],
@@ -97,7 +108,7 @@ def register_order(request):
             client_phone=get_orders['phonenumber'],
             client_address=get_orders['address'],
         )
-        print('order = ',order)
+        print('order = ', order)
         for product_element in get_orders['products']:
             product_by_id = Product.objects.get(id=product_element['product'])
             product_quantity = product_element['quantity']
@@ -107,5 +118,6 @@ def register_order(request):
 
             order.order_elements.add(new_product)
         return Response()
-    except ValueError:
-        return Response({'error': 'Error order register'})
+
+    except Exception as exc:
+        return Response(f'error: {exc}', status=status.HTTP_400_BAD_REQUEST)
