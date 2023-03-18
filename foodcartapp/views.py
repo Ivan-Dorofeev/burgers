18284102a193117a161678511333386c13ea62ef
Ivan Dorofeev <axxel123@rambler.ru>
2023-03-18 11:13:54 +0300

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,7 +1,9 @@
 import json
+import traceback
 
 from django.http import JsonResponse
 from django.templatetags.static import static
+from phonenumber_field.phonenumber import PhoneNumber
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -91,17 +93,40 @@ def register_order(request):
     try:
         get_orders = request.data
         print('orders = ', get_orders)
-        print('orders.type = ', type(get_orders))
 
-        if not isinstance(get_orders['products'], list):
-            return Response('products: Ожидался list со значениями, но был получен "str"', status=status.HTTP_400_BAD_REQUEST)
-        elif get_orders['products'] == 'null':
-            return Response('products: Это поле не может быть пустым',status=status.HTTP_400_BAD_REQUEST)
-        elif not get_orders['products']:
-            return Response('products: Этот список не может быть пустым', status=status.HTTP_400_BAD_REQUEST)
-        elif not 'products' in get_orders.keys():
-            return Response('products: Обязательное поле', status=status.HTTP_400_BAD_REQUEST)
+        """Проверки"""
+        order_key_list = list(get_orders.keys())
+        product_ids_from_bd = [product.id for product in Product.objects.all()]
+        print('product_ids_from_bd = ', product_ids_from_bd)
 
+        if not all(
+            list(map(lambda x: x in order_key_list, ['products', 'firstname', 'lastname', 'phonenumber', 'address']))):
+            return Response('products, firstname, lastname, phonenumber, address: Обязательные поля',
+                            status=status.HTTP_200_OK)
+
+        elif not all(
+            [False for i in ['products', 'firstname', 'lastname', 'phonenumber', 'address'] if not get_orders[i]]):
+            return Response('products, firstname, lastname, phonenumber, address: Эти поля не могут быть пустыми.',
+                            status=status.HTTP_200_OK)
+
+        elif isinstance(get_orders['firstname'], list):
+            return Response(f'firstname: Not a valid string.', status=status.HTTP_200_OK)
+
+        elif not isinstance(get_orders['products'], list):
+            return Response("products: Ожидался list со значениями, но был получен 'str'", status=status.HTTP_200_OK)
+
+        check_number = PhoneNumber.from_string(get_orders['phonenumber'])
+        if not check_number.is_valid():
+            return Response('phonenumber: Введен некорректный номер телефона.', status=status.HTTP_200_OK)
+
+        order_product_ids = get_orders['products']
+        for order_product_id in order_product_ids:
+            order_product_id_number = order_product_id['product']
+            if order_product_id_number not in product_ids_from_bd:
+                return Response(f'products: Недопустимый первичный ключ {order_product_id_number}',
+                                status=status.HTTP_200_OK)
+
+        """Запись в БД"""
         order = Order.objects.create(
             client_name=get_orders['firstname'],
             client_surname=get_orders['lastname'],
@@ -120,4 +145,5 @@ def register_order(request):
         return Response()
 
     except Exception as exc:
+        print(traceback.format_exc())
         return Response(f'error: {exc}', status=status.HTTP_400_BAD_REQUEST)
